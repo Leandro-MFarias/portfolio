e1f3476a0d8a6de5228a1ec5715c13941fa39ff4
Leandro Farias <leandrorf1606@gmail.com>
2024-03-21 16:55:10 -0300

    adicionando seta no hero

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -2,12 +2,14 @@ import { useTypingEffect } from '../../hooks/useTypingEffect'
 import Header from '../Header'
 import './hero.css'
 import video from '../../assets/universe-blue.mp4'
+import { MdOutlineKeyboardArrowDown } from "react-icons/md";
+
+
 function Hero() {
     const dev = useTypingEffect("Front-End Developer", 100)
 
     return (
         <section className='hero'>
-
             <video autoPlay muted loop className="video-background">
                 <source src={video} type="video/mp4" />
             </video>
@@ -22,8 +24,11 @@ function Hero() {
                     </div>
                     <h1>Leandro Farias</h1>
                     <div className='text-dev'>{dev}</div>
-
                 </div>
+
+                <a href="#about-me" className='arrow'>
+                    <MdOutlineKeyboardArrowDown />
+                </a>
             </div>
         </section>
     )
