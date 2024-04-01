dfbaa9fe2caf2910e313e0d526351be8a281ee4f
Leandro Farias <leandrorf1606@gmail.com>
2024-04-01 19:58:18 -0300

    Adicionando section project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import About from "./components/About"
 import Hero from "./components/Hero"
+import Projects from "./components/Projects"
 import Skills from "./components/Skills"
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Hero />
       <About />
       <Skills />
+      <Projects />
     </>
   )
 }
---
+++ src/components/Projects/index.jsx
@@ -0,0 +1,96 @@
+import "./projects.css"
+
+function Projects () {
+    return (
+        <section className="Projects section">
+            <div className="container">
+                <div className="flex-title">
+                    <h2>Projetos</h2>
+                </div>
+            </div>
+
+            <div className="container-projects">
+
+                <div className="project">
+                    <a href="#">
+                        <img src="https://via.placeholder.com/515x700" alt="XXXXXXXXXX" />
+                        <h3>Nome Projeto</h3>
+                        <div className="info-project">
+                            <p>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat, sed? Rem deleniti perspiciatis, in maxime exercitationem maiores, officia beatae
+                            </p>
+                            <p>🔗Ver no Github Pages</p>
+                        </div>
+                    </a>
+                </div>
+
+                <div className="project">
+                    <a href="#">
+                        <img src="https://via.placeholder.com/515x700" alt="XXXXXXXXXX" />
+                        <h3>Nome Projeto</h3>
+                        <div className="info-project">
+                            <p>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat, sed? Rem deleniti perspiciatis, in maxime exercitationem maiores, officia beatae, aliquid vel hic distinctio impedit velit repellendus. Alias amet laborum delectus.
+                            </p>
+                            <p>🔗Ver no Github Pages</p>
+                        </div>
+                    </a>
+                </div>
+
+                <div className="project">
+                    <a href="#">
+                        <img src="https://via.placeholder.com/515x700" alt="XXXXXXXXXX" />
+                        <h3>Nome Projeto</h3>
+                        <div className="info-project">
+                            <p>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat, sed? Rem deleniti perspiciatis, in maxime exercitationem maiores, officia beatae, aliquid vel hic distinctio impedit velit repellendus. Alias amet laborum delectus.
+                            </p>
+                            <p>🔗Ver no Github Pages</p>
+                        </div>
+                    </a>
+                </div>
+
+                <div className="project">
+                    <a href="#">
+                        <img src="https://via.placeholder.com/515x700" alt="XXXXXXXXXX" />
+                        <h3>Nome Projeto</h3>
+                        <div className="info-project">
+                            <p>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat, sed? Rem deleniti perspiciatis, in maxime exercitationem maiores, officia beatae, aliquid vel hic distinctio impedit velit repellendus. Alias amet laborum delectus.
+                            </p>
+                            <p>🔗Ver no Github Pages</p>
+                        </div>
+                    </a>
+                </div>
+
+                <div className="project">
+                    <a href="#">
+                        <img src="https://via.placeholder.com/515x700" alt="XXXXXXXXXX" />
+                        <h3>Nome Projeto</h3>
+                        <div className="info-project">
+                            <p>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat, sed? Rem deleniti perspiciatis, in maxime exercitationem maiores, officia beatae, aliquid vel hic distinctio impedit velit repellendus. Alias amet laborum delectus.
+                            </p>
+                            <p>🔗Ver no Github Pages</p>
+                        </div>
+                    </a>
+                </div>
+
+                <div className="project">
+                    <a href="#">
+                        <img src="https://via.placeholder.com/515x700" alt="XXXXXXXXXX" />
+                        <h3>Nome Projeto</h3>
+                        <div className="info-project">
+                            <p>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat, sed? Rem deleniti perspiciatis, in maxime exercitationem maiores, officia beatae, aliquid vel hic distinctio impedit velit repellendus. Alias amet laborum delectus.
+                            </p>
+                            <p>🔗Ver no Github Pages</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Projects
