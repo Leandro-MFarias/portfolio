ed5a0926b1917ad754228f5ad60e1feddf61873c
Leandro Farias <leandrorf1606@gmail.com>
2024-04-01 21:34:28 -0300

    Iniciando Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import About from "./components/About"
+import Footer from "./components/Footer"
 import Hero from "./components/Hero"
 import Projects from "./components/Projects"
 import Skills from "./components/Skills"
@@ -10,6 +11,7 @@ function App() {
       <About />
       <Skills />
       <Projects />
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,17 @@
+import './footer.css'
+
+function Footer() {
+    return (
+        <section className='footer'>
+            <div className="container">
+                <h2>Leandro Marinaci</h2>
+
+                <div className="social-midia">
+                    
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Footer
