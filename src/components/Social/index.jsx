9c3a2ec6bde1e4ce3dd18cc8ab90b0a273ede4cf
Leandro Farias <leandrorf1606@gmail.com>
2024-04-06 13:39:13 -0300

    social inner-shadow

--- src/components/Social/index.jsx
+++ src/components/Social/index.jsx
@@ -6,19 +6,19 @@ import { FaWhatsapp } from "react-icons/fa";
 function Social() {
     return (
         <div className="social-midia">
-            <a href="https://www.linkedin.com/in/leandro-farias-dev/" target="_blank" className="outer-shadow">
+            <a href="https://www.linkedin.com/in/leandro-farias-dev/" target="_blank" className="inner-social">
                 <FaLinkedinIn />
             </a>
 
-            <a href="https://github.com/Leandro-MFarias" target="_blank" className="outer-shadow">
+            <a href="https://github.com/Leandro-MFarias" target="_blank" className="inner-social">
                 <FaGithub />
             </a>
 
-            <a href="https://www.instagram.com/leandro_marinaci?igsh=bG1yaWZqeGhxazc=" target="_blank" className="outer-shadow">
+            <a href="https://www.instagram.com/leandro_marinaci?igsh=bG1yaWZqeGhxazc=" target="_blank" className="inner-social">
                 <FaInstagram />
             </a>
 
-            <a href="https://wa.link/jsofvi" target="_blank" className="outer-shadow">
+            <a href="https://wa.link/jsofvi" target="_blank" className="inner-social">
                 <FaWhatsapp />
             </a>
         </div>
